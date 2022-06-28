d94d3cda4cec6536dbb1e4c2429eca0afc8cc836
Rumerasio <joseph0417@naver.com>
2022-06-28 09:25:34 +0900

    first

---
+++ src/java_personal/Hello.java
@@ -0,0 +1,11 @@
+package java_personal;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github!");
+		System.out.println("Any problem?");  
+	}
+
+}

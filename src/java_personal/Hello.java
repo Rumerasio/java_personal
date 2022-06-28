4e57547450cf61bf7f3972c58c6ef8537e837811
Rumerasio <joseph0417@naver.com>
2022-06-28 11:02:10 +0900

    adding multi

--- src/java_personal/Hello.java
+++ src/java_personal/Hello.java
@@ -6,6 +6,14 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Github!");
 		System.out.println("Any problem?");  
+		
+		for(int i =2;i<=9;i++) {
+			System.out.println(i+"단");
+			for(int j =1;j<=9;j++) {
+				int r=i*j;
+				System.out.println(i+" X "+j +" = "+ r);
+			}
+		}
 	}
 
 }

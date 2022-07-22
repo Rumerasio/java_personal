406e2e4f5eb4dfb2c7225fee22e7a92987a9ccc5
Rumerasio <joseph0417@naver.com>
2022-07-22 13:45:21 +0900

    refresh

---
+++ src/java_personal/AddAlgorithm.java
@@ -0,0 +1,13 @@
+package java_personal;
+
+public class AddAlgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=100; 
+		int b=200;
+		System.out.println(a+"+"+b+"="+(a+b));
+
+	}
+
+}
